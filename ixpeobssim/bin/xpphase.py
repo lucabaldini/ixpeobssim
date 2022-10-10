32a677050ca90e24adf67a3107c830bc202aa460
Stefano Silvestri <silvestri.sfn@gmail.com>
2022-10-10 12:02:31 +0200

    tentative fix to issue 651

--- ixpeobssim/bin/xpphase.py
+++ ixpeobssim/bin/xpphase.py
@@ -132,7 +132,11 @@ def xpphase(**kwargs):
         evt_header = evt_hdu.header
 
         logger.info('Calculating pulsar phase...')
-        phase = eph.fold(time_, kwargs.get('met0'), kwargs.get('phi0'))
+        if kwargs.get('met0') is None:
+            met0 = evt_header['TSTART']
+        else:
+            met0 = kwargs.get('met0')
+        phase = eph.fold(time_, met0, kwargs.get('phi0'))
         logger.info('Creating phase column...')
         col_ = fits.Column(name='PHASE', array=phase, format='E')
         new_hdu = fits.BinTableHDU.from_columns(evt_hdu.data.columns + col_, header=evt_header)
