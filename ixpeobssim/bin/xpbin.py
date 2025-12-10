e7f9d98ea606639db0229841e3b67ea07a48d141
Alberto Manfreda <albertom@infn.it>
2025-12-10 15:11:48 +0100

    Minor

--- ixpeobssim/bin/xpbin.py
+++ ixpeobssim/bin/xpbin.py
@@ -164,8 +164,8 @@ def subtract_flare_pcube(evt_binned, evt_backscal, inecl_out, insun_out,
     read = BINNING_READ_DICT['PCUBE']
     inecl = read(inecl_out)
     insun = read(insun_out)
-    insun_lt = insun.primary_header['LIVETIME']
-    inecl_lt = inecl.primary_header['LIVETIME']
+    insun_lt = insun.event_header['LIVETIME']
+    inecl_lt = inecl.event_header['LIVETIME']
     # Normalize inecl to insun livetime
     inecl *= (insun_lt / inecl_lt)
     # Flare content is contained only in the insun part, no need to rescale
@@ -199,8 +199,8 @@ def subtract_flare_map(evt_binned, evt_backscal, algorithm, inecl_out,
     read = BINNING_READ_DICT[algorithm]
     inecl = read(inecl_out)
     insun = read(insun_out)
-    insun_lt = insun.primary_header['LIVETIME']
-    inecl_lt = inecl.primary_header['LIVETIME']
+    insun_lt = insun.event_header['LIVETIME']
+    inecl_lt = inecl.event_header['LIVETIME']
     # Normalize inecl to insun livetime
     inecl *= (insun_lt / inecl_lt)
     # Flare content is contained only in the insun part, no need to rescale
